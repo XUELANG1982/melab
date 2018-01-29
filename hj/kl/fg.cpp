c48277e200076161cf41290429c1e814349c5407
XUELANG1982 <XUJING5031677@SINA.COM>
2018-01-29 21:35:46 +0800

    list

--- hj/kl/fg.cpp
+++ hj/kl/fg.cpp
@@ -2,7 +2,16 @@
 using namespace std;
 void op()
 {
-    int
+    int i,j,sum;
+    sum=0;
+    for(i=1;i<=9;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            sum=j*i;
+            cout<<sum<<endl;
+        }
+    }
 }
 int main()
 {
